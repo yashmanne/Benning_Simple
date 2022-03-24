00692650d5b188894381f32f8af0daae95804574
Yash Manne <manneyas@dev-intel16.i>
2022-03-24 14:16:57 -0400

    Updated Script

--- input/import_data.sh
+++ input/import_data.sh
@@ -4,27 +4,31 @@
 
 dataFolder="../../210409_sequencing"
 
+# Rename output file
+
+lineName="number_twelve"
+
 # mut R1
 
-gunzip -c $dataFolder/A/*L02*_1* $dataFolder/A/*L03*_1* $dataFolder/A/*L04*_1* > number_twelve.mut.R1.fastq
+gunzip -c $dataFolder/A/*L02*_1* $dataFolder/A/*L03*_1* $dataFolder/A/*L04*_1* > $lineName.mut.R1.fastq
 
-gzip number_twelve.mut.R1.fastq
+gzip $lineName.mut.R1.fastq
 
 # mut R2
 
-gunzip -c $dataFolder/A/*L02*_2* $dataFolder/A/*L03*_2* $dataFolder/A/*L04*_2* > number_twelve.mut.R2.fastq
+gunzip -c $dataFolder/A/*L02*_2* $dataFolder/A/*L03*_2* $dataFolder/A/*L04*_2* > $lineName.mut.R2.fastq
 
-gzip number_twelve.mut.R2.fastq
+gzip $lineName.mut.R2.fastq
 
 # wt R1
 
-gunzip -c $dataFolder/B/*L02*_1* $dataFolder/B/*L03*_1* $dataFolder/B/*L04*_1* > number_twelve.wt.R1.fastq
+gunzip -c $dataFolder/B/*L02*_1* $dataFolder/B/*L03*_1* $dataFolder/B/*L04*_1* > $lineName.wt.R1.fastq
 
-gzip number_twelve.wt.R1.fastq
+gzip $lineName.wt.R1.fastq
 
 # wt R2
 
-gunzip -c $dataFolder/B/*L02*_2* $dataFolder/B/*L03*_2* $dataFolder/B/*L04*_2* > number_twelve.wt.R2.fastq
+gunzip -c $dataFolder/B/*L02*_2* $dataFolder/B/*L03*_2* $dataFolder/B/*L04*_2* > $lineName.wt.R2.fastq
 
-gzip number_twelve.wt.R2.fastq
+gzip $lineName.wt.R2.fastq
 
