a868aa2c2acfedf623f5098ec684ae7b95a39b9b
Yash Manne <manneyas@dev-intel16.i>
2022-05-02 19:20:11 -0400

    finalized scripts

---
+++ variables.sh
@@ -0,0 +1,11 @@
+#### Change these to what yours are
+
+# data folder
+dataLocation="../210409_sequencing"
+# output file
+lineName="number_twelve"
+
+#### Note that in the example case, data was spread across 3 lanes L02, L03, L04. This may not always be the case and might need to be modified
+lane1="L02"
+lane2="L03"
+lane3="L04"
