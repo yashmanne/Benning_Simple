e2f107e19bdb89deb8e85b91f385612d9668473d
Yash Manne <manneyas@dev-intel16.i>
2022-05-03 17:00:47 -0400

    updated runScripts.sh

--- runScripts.sh
+++ runScripts.sh
@@ -2,14 +2,14 @@
 cd ./scripts
 ### argument to say whether or not to collect data.
 arg=$1
-### if need to collect data:
 if [ $arg = "data" ]
-then 
-    echo "Importing data!"
-    sbatch import_data.sb
-else
-### if data is already present in ./input folder
-# exists for cases where modifications need to be made
+then
+    ### if data is already present in ./input folder
+    # exists for cases where modifications need to be made
     echo "Data is preloaded and starting pipeline!"
     sbatch getReferences_Arabidopsis.sb
+else
+    ### if need to collect data:
+    echo "Importing data!"
+    sbatch import_data.sb
 fi
